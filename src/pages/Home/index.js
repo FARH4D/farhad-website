1a3d9947988386d2830929e796581283a461041c
FARH4D <FarhadAhmed24@hotmail.co.uk>
2023-09-29 14:55:38 +0100

    Added animations

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,16 @@
 import './index.scss'
+import Loader from 'react-loaders'
+import { motion } from 'framer-motion'
 
 const Home = () => {
     return  (
-        <div className='home-container'>
+        <>
+        <motion.div className='home-container'
+            transition={{delay: 2}}
+            initial= {{opacity: 0, y: 20}}
+            animate = {{opacity: 1, y: 20}}
+            exit={{opacity: 0, y: 20}}
+        >
             <div className='text'>
                 <h1> Hello, welcome to my website. </h1>
         
@@ -10,7 +18,16 @@ const Home = () => {
 
 
 
-        </div>
+        </motion.div>
+        <motion.div
+            transition={{delay: 0.3}}
+            initial= {{opacity: 0}}
+            animate = {{opacity: 1}}
+            exit={{opacity: 0}}
+        >
+        <Loader type = "ball-grid-pulse" style={{transform: 'scale(3'}} />
+        </motion.div>
+        </>
     )
 }
 
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,16 +1,36 @@
 import './index.scss'
+import { motion } from 'framer-motion'
+import Loader from 'react-loaders'
 
 const Projects = () => {
     return (
-        <div className='home-container'>
+        <>
+        <motion.div
+            className='container projects-page'
+            transition={{delay: 2}}
+            initial= {{opacity: 0}}
+            animate = {{opacity: 1}}
+            exit={{opacity: 0}}
+        >
             <div className='text'>
-                <h1> Hello dudeeee </h1>
+                <h1> Projects </h1>
+
+                <p>
+                    hi
+                </p>
+
 
             </div>
-
-
-
-        </div>
+        </motion.div>
+        <motion.div
+            transition={{delay: 0.3}}
+            initial= {{opacity: 0}}
+            animate = {{opacity: 1}}
+            exit={{opacity: 0}}
+        >
+        <Loader type = "ball-grid-pulse" style={{transform: 'scale(3'}} />
+        </motion.div>
+        </>
     )
 }
 
